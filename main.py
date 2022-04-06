51a6f760e9b7a0d05473fed62764a1b1a07306f8
paulorsgoncalves <paulo.goncalves@live.com>
2022-04-06 14:05:02 -0300

    meusite_v01

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html')
 
 @app.route('/contato')
 def contato():
-    return 'Qualquer dúvida mande um email pra nois'
+    return render_template('contato.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
