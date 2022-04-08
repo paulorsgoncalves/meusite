f8b5c6ac51218d4e207b6f994f13f0aaeadf54ff
paulorsgoncalves <paulo.goncalves@live.com>
2022-04-08 10:54:19 -0300

    19 concluido

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
@@ -23,6 +23,12 @@ def usuarios():
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
+    if form_login.validate_on_submit() and 'botao_login' in request.form:
+        flash(f'Login feito com sucesso no email: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    if form_criarconta.validate_on_submit() and 'botao_submit' in request.form:
+        flash(f'Conta criada para o email: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 if __name__ == '__main__':
