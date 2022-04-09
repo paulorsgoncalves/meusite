18970b29ec48ff7e0ebff3731416e40b95387f8a
paulorsgoncalves <paulo.goncalves@live.com>
2022-04-09 00:33:09 -0300

    login correto

--- forms.py
+++ forms.py
@@ -5,12 +5,12 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class FormCriarConta(FlaskForm):
     username = StringField('Nome de Usuário', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     confirmacao = PasswordField('Confirmaçao da Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit = SubmitField('Criar Conta')
 
 class FormLogin(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     lembrar_dados = BooleanField('Lembrar Dados')
     botao_login = SubmitField('Fazer Login')
