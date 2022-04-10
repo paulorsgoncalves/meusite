e159ed5472f97f9be35527136d0d18517ce6df9c
paulorsgoncalves <paulo.goncalves@live.com>
2022-04-10 17:42:56 -0300

    ultimo dessa ramificação

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 lista_usuarios = ['Paulo', 'Roberto', 'Livia', 'Teresinha']
 
 app.config['SECRET_KEY'] = 'e649fa424ae88f4de15c8126a1ef2f33'
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///meusite.db'
+
+database = SQLAlchemy(app)
 
 @app.route("/")
 def home():
