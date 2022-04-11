dd96c0eb792405812f6058dd8eb0ab8f6b2eeba1
paulorsgoncalves <paulo.goncalves@live.com>
2022-04-10 23:46:53 -0300

    banco de dados criado

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 lista_usuarios = ['Paulo', 'Roberto', 'Livia', 'Teresinha']
 
 app.config['SECRET_KEY'] = 'e649fa424ae88f4de15c8126a1ef2f33'
-app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///meusite.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 
---
+++ teste.py
@@ -0,0 +1,4 @@
+from main import database
+from models import Usuario, Post
+
+database.create_all()
